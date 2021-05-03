62097510e5a092d7b03dc29e47446c5507bb0615
Eugene <Eugeniolio@yandex.com>
2021-05-03 14:20:30 +0300

    2.1

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -3,7 +3,7 @@
 
 import UIKit
 
-class ViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate
+class ViewController: UIViewController , UIPickerViewDataSource
 {
   // Initializing IBO Outlets
     @IBOutlet weak var bitcoinLabel: UILabel!
@@ -17,10 +17,28 @@ class ViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDel
         super.viewDidLoad()
         
         currencyPicker.dataSource = self
-        
         currencyPicker.delegate = self
+
         coinManager.delegate = self
     }
+}
+
+
+// MARK: - ViewControoler CoinManagerDelegate protocol
+
+extension ViewController: CoinManagerDelegate {
+    // Delegate function
+    func didUpdateView(_ result: CurrencyStruct) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = String(format:"%.1f",result.currencyRate)
+            self.currencyLabel.text = result.currencyName
+        }
+    }
+}
+
+// MARK: - UIPickerViewDelegate methods
+
+extension ViewController: UIPickerViewDelegate {
     
     // Add delegate method pickerView
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -30,8 +48,8 @@ class ViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDel
     // Add delegate method for pickerView
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let currencyLiteral = coinManager.currencyArray[row]
-        
         coinManager.getCoinPrice(for: currencyLiteral)
+        
     }
     
     // Add function which return number of components
@@ -39,20 +57,8 @@ class ViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDel
         return 1
     }
     
-    
     // Function which return number of elements in currency Array
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
-    
-    // Delegate function
-    func didUpdateView(_ result: CurrencyStruct) {
-        DispatchQueue.main.async {
-            self.bitcoinLabel.text = String(format:"%.1f",result.currencyRate)
-            self.currencyLabel.text = result.currencyName
-        }
-    }
-
-
 }
-
